b7a70a3a1d8da0932fd7abb5a51daf83ded7f260
Karim Fawzy <karim.fawzy180@gmail.com>
2026-05-24 17:58:59 +0300

    feat(settings): add splash screen shortcut button

--- lib/features/settings/presentation/screens/settings_screen.dart
+++ lib/features/settings/presentation/screens/settings_screen.dart
@@ -4,12 +4,14 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../../core/constants/app_constants.dart';
 import '../../../../core/constants/image_constants.dart';
+import '../../../../core/routing/app_routes.dart';
 import '../../../../core/settings/app_settings_controller.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/theme/app_spacing.dart';
 import '../../../../core/theme/app_text_styles.dart';
 import '../../../../core/widgets/app_scaffold.dart';
 import '../../../../core/widgets/app_scroll_view.dart';
+import '../../../../core/widgets/secondary_button.dart';
 import '../../../../shared/widgets/screen_header.dart';
 import '../widgets/settings_tile.dart';
 
@@ -173,6 +175,13 @@ class _SettingsBody extends StatelessWidget {
                     ),
                   ),
                 ],
+              ),
+              SizedBox(height: AppSpacing.stackMd.h),
+              SecondaryButton(
+                label: 'Open splash screen',
+                onPressed: () {
+                  Navigator.of(context).pushNamed(AppRoutes.splash);
+                },
               ),
               SizedBox(height: AppSpacing.stackLg.h),
             ],
